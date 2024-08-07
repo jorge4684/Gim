cbaa0f008df0eb57223ccbe8b3132dd46b5be9cc
jorge4684 <hernandezjs2009@gmail.com>
2024-08-07 13:31:16 -0600

    login y register 50% falta el google y facebook y otros login y regitros automaticos

--- Gim/src/Pages/Register.jsx
+++ Gim/src/Pages/Register.jsx
@@ -1,9 +1,53 @@
-//import React from 'react'
+import background from "../assets/background.png";
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <>
+      <div className="flex grid-cols-2 bg-gray-200">
+        <div>
+          <img
+            src={background}
+            alt=""
+            className="hidden md:flex h-screen w-full "
+          />
+        </div>
+        <div className="flex flex-col items-center place-content-center w-full">
+          <h2 className="text-center text-5xl font-semibold mt-44">Register</h2>
+          <form className="flex flex-col mt-16 h-screen items-center">
+            <input
+              type="text"
+              placeholder="first name"
+              className="w-72 h-8 bg-transparent border border-gray-400"
+            />
+            <input
+              type="text"
+              placeholder="last name"
+              className="w-72 h-8 mt-4 bg-transparent border border-gray-400"
+            />
+            <input
+              type="email"
+              placeholder="emal"
+              className="w-72 h-8 mt-4 bg-transparent border border-gray-400"
+            />
+            <input
+              type="password"
+              placeholder="password"
+              className="w-72 h-8 mt-4 bg-transparent border border-gray-400"
+            />
+            <buttom className="flex w-72 mt-4 h-10 rounded-lg text-center items-center place-content-center text-white bg-red-700">
+              Register
+            </buttom>
+            <p>
+              You do not have an account{" "}
+              <a href="./Login" className="text-red-900">
+                Login Here
+              </a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Register
+export default Register;
